106299c1e47e82dda5d15bad23476e7ab3b195d4
Tim Huitt <timhuitt@gmail.com>
2024-04-13 19:34:48 -0600

    add about me

--- src/app/components/Body.tsx
+++ src/app/components/Body.tsx
@@ -23,6 +23,39 @@ const Body: React.FC = () => {
 
       </div>
       <Gallery />
+      <h1 className={`${sketch.className} text-[6rem] font-thin mt-40 text-purple-950`}>About Me</h1>
+      <div className="flex flex-col items-center justify-center w-[90%] md:w-5/6 lg:w-4/6 bg-teal-400 rounded-xl overflow-y-auto">
+        <p className="text-xl p-6">My name is Steph Huitt and also known as aDoodlingOctopus on social media, I am an illustrator painter that specializes in whimsical colorful designs that bring smiles to faces of all ages! I love to create a sense of joy through the worlds I create and what I bring to life! </p>
+        <div className="flex justify-center gap-10 w-full h-[250px] my-6  overflow-hidden">
+          <div className="w-[25%] h-[250px] bg-yellow-300 rounded-xl overflow-hidden">
+            <Image 
+              src={"/images/featured/1.png"}
+              alt={""}
+              width={1000}
+              height={50}
+              style={{objectFit: "contain"}}
+            />
+          </div>
+          <div className="w-[25%] h-[250px] bg-yellow-300 rounded-xl overflow-hidden">
+            <Image 
+              src={"/images/featured/3.png"}
+              alt={""}
+              width={1000}
+              height={50}
+              style={{objectFit: "contain"}}
+            />
+          </div>
+          <div className="w-[25%] h-[250px] bg-yellow-300 rounded-xl overflow-hidden">
+            <Image 
+              src={"/images/featured/6.png"}
+              alt={""}
+              width={1000}
+              height={50}
+              style={{objectFit: "contain"}}
+            />
+          </div>
+        </div>
+      </div>
     </>
   )
 }
--- src/app/components/Gallery.tsx
+++ src/app/components/Gallery.tsx
@@ -2,7 +2,7 @@ import Images from "./Images"
 const Gallery = () => {
 
   return (
-    <div className="flex">
+    <div className="flex max-w-screen">
       <Images color={'#C4B5FD'} rotation={12} index={40} image={'1.png'} />
       <Images color={'#6EE7B7'} rotation={6} index={30} image={'2.png'} />
       <Images color={'#C4B5FD'} rotation={-6} index={40} image={'3.png'} />
