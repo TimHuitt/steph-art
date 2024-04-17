c84ac1cb00ea3bec27f3bb62d7d1dc3121cfbb28
Tim Huitt <timhuitt@gmail.com>
2024-04-17 11:03:40 -0600

    add scrolling announcements

--- src/app/components/Announcements.tsx
+++ src/app/components/Announcements.tsx
@@ -1,9 +1,13 @@
+import { useState } from 'react'
 
-const Announcements: React.FC = () => {
+const Announcements: React.FC<{width: number}> = ({ width }) => {
+  const [ showDetails, setShowDetails ] = useState<boolean>(false)
   
   return (
-    <div className="w-[75%] min-h-8 flex justify-center items-center mt-2 md:mt-4 text-sm md:text-base lg:text-lg bg-purple-950 opacity-8 rounded-xl">
-      <p className="text-gray-100">Announcements Will Appear Here! :D</p>
+    <div className="relative w-[75%] min-h-8 flex justify-center items-center mt-2 md:mt-4 text-sm md:text-base lg:text-lg bg-purple-950 opacity-8 rounded-xl whitespace-nowrap overflow-hidden cursor-pointer">
+      <p className="ticker absolute inline-block text-gray-100">
+        LIVE PAINTING: Come see my live painting night at the OZO Latte Art Throwdown! Click here for details!
+      </p>
     </div>
   )
 }
