5600aabd662859db1447f70d6a182ca4e78f4970
Tim Huitt <timhuitt@gmail.com>
2024-04-17 11:11:02 -0600

    test announcements window

--- src/app/components/Announcements.tsx
+++ src/app/components/Announcements.tsx
@@ -4,11 +4,16 @@ const Announcements: React.FC<{width: number}> = ({ width }) => {
   const [ showDetails, setShowDetails ] = useState<boolean>(false)
   
   return (
-    <div className="relative w-[75%] min-h-8 flex justify-center items-center mt-2 md:mt-4 text-sm md:text-base lg:text-lg bg-purple-950 opacity-8 rounded-xl whitespace-nowrap overflow-hidden cursor-pointer">
-      <p className="ticker absolute inline-block text-gray-100">
-        LIVE PAINTING: Come see my live painting night at the OZO Latte Art Throwdown! Click here for details!
-      </p>
-    </div>
+    <>
+      <div className="relative w-[75%] min-h-8 flex justify-center items-center mt-2 md:mt-4 text-sm md:text-base lg:text-lg bg-purple-950 opacity-8 rounded-xl whitespace-nowrap overflow-hidden cursor-pointer">
+        <p className="ticker absolute inline-block text-gray-100">
+          LIVE PAINTING: Come see my live painting night at the OZO Latte Art Throwdown! Click here for details!
+        </p>
+      </div>
+      {/* <div className='fixed top-0 left-0 h-screen w-screen min-w-screen min-h-screen bg-purple-950 z-40'>
+        
+      </div> */}
+    </>
   )
 }
 
