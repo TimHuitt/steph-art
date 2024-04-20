9dd7d51ca851c41f7fc2a4566e25222ea07a932f
Tim Huitt <timhuitt@gmail.com>
2024-04-20 13:21:11 -0600

    update about

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -16,8 +16,8 @@ const About: React.FC = () => {
       <h1 ref={aboutRef} className={`${sketch.className} text-purple-950 mt-40`}>About Me</h1>
       <div className="flex flex-col items-center justify-center w-[90%] md:w-5/6 lg:w-4/6 bg-teal-400 rounded-xl">
         <p className="text-2xl py-8 px-12 w-full text-center">My name is Steph Huitt and also known as aDoodlingOctopus on social media, I am an illustrator painter that specializes in whimsical colorful designs that bring smiles to faces of all ages! I love to create a sense of joy through the worlds I create and what I bring to life! </p>
-        <div className="relative flex justify-center items-start h-[600px] gap-6 w-full my-6 px-6 overflow-hidden">
-          <div className="relative w-1/4 h-2/3 rounded-xl overflow-hidden">
+        <div className="relative grid grid-rows-[20%_60%_20%] grid-cols-2 md:grid-rows-1 md:grid-cols-3 justify-center items-start h-[600px] gap-6 w-full my-6 px-6 overflow-hidden">
+          <div className="relative w-full h-full rounded-xl overflow-hidden">
             <Image 
               src={"/images/featured/11.jpg"}
               alt={""}
@@ -25,7 +25,7 @@ const About: React.FC = () => {
               style={{objectFit: "cover"}}
             />
           </div>
-          <div className="relative w-3/5 h-full rounded-xl overflow-hidden">
+          <div className="relative w-full h-full rounded-xl overflow-hidden">
             <Image 
               src={"/images/featured/12.jpg"}
               alt={""}
@@ -33,7 +33,7 @@ const About: React.FC = () => {
               style={{objectFit: "cover"}}
             />
           </div>
-          <div className="relative w-1/4 h-2/3 rounded-xl overflow-hidden">
+          <div className="relative w-full h-4/5 rounded-xl overflow-hidden">
             <Image 
               src={"/images/featured/13.jpg"}
               alt={""}
